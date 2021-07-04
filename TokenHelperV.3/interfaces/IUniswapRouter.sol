25dbeb532351bc66197d6aa0774315e18301ccc9
enjoyproth <skynoy1@hotmail.com>
2021-07-05 05:18:40 +0700

    Delete TokenHelperV.3 directory

--- TokenHelperV.3/interfaces/IUniswapRouter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.6.12;
-
-interface IUniswapRouter {
-    function factory() external pure returns (address);
-    function WETH() external pure returns (address);
-    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);
-}
